9e44c377df0508694d707dde329948c3dbd158a9
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-04 00:46:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,7 +26,7 @@ class Application
       add_term = req.params["item"]
       if @@items.include?(add_term)
         @@cart.push(add_term)
-        resp.write "added #{item_to_add}"
+        resp.write "added #{add_term}"
       else
         resp.write "We don't have that item"
       end

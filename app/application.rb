b3f4a0431f9d8a42ed1b7e62f4519a469840f449
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-04 00:43:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,6 +26,7 @@ class Application
       add_term = req.params["item"]
       if @@items.include?(add_term)
         @@cart.push(add_term)
+        @@cart
       else
         resp.write "We don't have that item"
       end
